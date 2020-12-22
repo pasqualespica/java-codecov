91fb8dfb77d935b45bfb4e720524fe239cd55e3e
pasqualespica <pasqualespica@gmail.com>
2020-12-22 09:43:21 +0100

    remove test

--- src/test/java/org/jacoco/examples/maven/java/HelloWorldTest.java
+++ src/test/java/org/jacoco/examples/maven/java/HelloWorldTest.java
@@ -7,21 +7,21 @@ import org.junit.Test;
 
 public class HelloWorldTest {
 
-	private HelloWorld subject;
+    private HelloWorld subject;
 
-	@Before
-	public void setup() {
-		subject = new HelloWorld();
-	}
-
-	@Test
-	public void testGetMessageFalse() {
-		assertEquals("Hello World!", subject.getMessage(false));
-	}
+    @Before
+    public void setup() {
+        subject = new HelloWorld();
+    }
 
     @Test
-	public void testGetMessageTrue() {
-		assertEquals("Hello Universe!", subject.getMessage(true));
-	}
+    public void testGetMessageFalse() {
+        assertEquals("Hello World!", subject.getMessage(false));
+    }
+
+    // @Test
+    // public void testGetMessageTrue() {
+    // assertEquals("Hello Universe!", subject.getMessage(true));
+    // }
 
 }
